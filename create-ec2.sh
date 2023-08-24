e766a24d4e759a1aaac22b198997b35a4862fa74
satyachiranjeevi <duvvu.chiranjeevi@gmail.com>
2023-08-24 20:42:24 +0100

    first commit

--- create-ec2.sh
+++ create-ec2.sh
@@ -11,14 +11,13 @@ SECURITY_GROUP_ID=sg-08aad2e7512ca2cf6
 
 for item in "${NAMES[@]}"
 do
-    if [ $item == "mongodb" || $item == "mysql" ]
+    if [ [ $item == "mongodb" || $item == "mysql" ] ]
     then
         INSTANCE_TYPE="t3.micro"
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    echo "Creating $item instance"
-
+    echo "creating $item instance...."
     aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids 
-    $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=A-$item}]"
+    $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$item}]"
 done
